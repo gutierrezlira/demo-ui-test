647dc4aba08a68e05b7c59c22054bb3e2b2ff9a8
Alina Darenskikh <alinadarenskikh@gmail.com>
2023-02-15 20:06:24 +0300

    add @Befor and @After in AmazonPage.java

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -1,45 +1,36 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AmazonTest {
-
-    @Test
-    public void testOpenAmazon(){
-
-        ChromeDriver driver = new ChromeDriver();
+    ChromeDriver driver = new ChromeDriver();
+    @Before
+    public void settingOpen() throws InterruptedException {
         driver.manage().window().maximize();
         driver.get(AmazonPage.URL_AMAZON);
-
+        Thread.sleep(3000);
+    }
+    @Test
+    public void testOpenAmazon(){
         WebElement navFooterElement = driver.findElement(By.xpath(AmazonPage.NAV_FOOTER_LINE));
-
         Assert.assertEquals(AmazonPage.footer_Text, navFooterElement.getText());
-        driver.quit();
     }
 
     @Test
     public void testOpenAmazonCart(){
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(AmazonPage.URL_AMAZON);
-
         WebElement cart = driver.findElement(By.xpath(AmazonPage.AMAZON_CART));
         cart.click();
 
         WebElement emptyCart = driver.findElement(By.xpath(AmazonPage.CART_IS_EMPTY));
         Assert.assertEquals(AmazonPage.CartEmptyText, emptyCart.getText());
-
-        driver.quit();
     }
 
     @Test
     public void testOpenAmazonLoginForm(){
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(AmazonPage.URL_AMAZON);
-
         WebElement cart = driver.findElement(By.xpath(AmazonPage.AMAZON_CART));
         cart.click();
 
@@ -49,16 +40,11 @@ public class AmazonTest {
         WebElement sighInTxt = driver.findElement(By.xpath(AmazonPage.SIGH_IN_TXT));
 
         Assert.assertEquals("Sign in", sighInTxt.getText());
-        driver.quit();
     }
 
 
     @Test
     public void testAmazonLoginFormWithEmptyCredentials(){
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(AmazonPage.URL_AMAZON);
-
         WebElement cart = driver.findElement(By.xpath(AmazonPage.AMAZON_CART));
         cart.click();
 
@@ -71,7 +57,10 @@ public class AmazonTest {
         WebElement errTxt = driver.findElement(By.xpath(AmazonPage.SIGH_IN_ERR_TXT));
 
         Assert.assertEquals("Enter your email or mobile phone number", errTxt.getText());
-        driver.quit();
 }
+    @After
+    public void setExit(){
+        driver.quit();
+    }
 
 }
