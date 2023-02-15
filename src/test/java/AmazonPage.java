756c0a0005374e29de6f60ae716dfddb6cc1ed5a
Alina Darenskikh <alinadarenskikh@gmail.com>
2023-02-15 20:15:23 +0300

    add @Befor and @After in OnlinerPage.java

--- src/test/java/AmazonPage.java
+++ src/test/java/AmazonPage.java
@@ -4,11 +4,8 @@ public class AmazonPage {
     public static String footer_Text = "© 1996-2023, Amazon.com, Inc. or its affiliates";
     public static final String AMAZON_CART = "//*[@id='nav-cart']";
     public static final String CART_IS_EMPTY = "//div[@id='sc-active-cart']//h2";
-
     public static String CartEmptyText ="Your Amazon Cart is empty";
-
     public static final String SIGN_IN_BTN = "//*[@id='a-autoid-0-announce']";
-
     public static final String SIGH_IN_TXT = "//div[@class='a-box']//h1";
     public static final String SIGH_IN_ERR_TXT = "//div[contains(text(),'Enter your email')]";
 }
